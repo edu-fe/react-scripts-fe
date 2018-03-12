f0224cef3b36f1735ad92fbf8bd6595c64b9d3a3
lgzhang <lgzhang@tfedu.net>
2018-03-12 20:18:14 +0800

    fix less

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -239,6 +239,21 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.less/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+               {
+                loader: require.resolve('less-loader')
+              },
+            ],
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
